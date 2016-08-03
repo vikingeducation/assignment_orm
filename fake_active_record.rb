2d55dba3c4156c58f37247a5e6140fc7a271e944
cjvirtucio87 <cjv287@gmail.com>
2016-08-03 06:37:23 -0700

    Nested BASE methods in singleton class.

--- fake_active_record.rb
+++ fake_active_record.rb
@@ -4,34 +4,37 @@ module FakeActiveRecord
     # model object
     # that returns pluralized
     # version of class name
-    def self.table_name
-      "#{self.name.downcase}s"
-    end
 
+    class << self
 
-    # gives you a hash of {column_name: column_type }
-    # for your table
-    def self.schema
-      return @schema if @schema
-
-      @schema = {}
-
-      # example:
-      # If you're a Post
-      # runs DB.table_info("posts")
-      DB.table_info(table_name) do |row|
-        @schema[row["name"]] = row["type"]
+      def table_name
+        "#{self.name.downcase}s"
       end
 
-      @schema
+
+      # gives you a hash of {column_name: column_type }
+      # for your table
+      def schema
+        return @schema if @schema
+
+        @schema = {}
+
+        # example:
+        # If you're a Post
+        # runs DB.table_info("posts")
+        DB.table_info(table_name) do |row|
+          @schema[row["name"]] = row["type"]
+        end
+
+        @schema
+      end
+
+      # convenience wrapper for your schema's column names
+      def columns
+        schema.keys
+      end
+
     end
-
-    # convenience wrapper for your schema's column names
-    def self.columns
-      schema.keys
-    end
-
-
 
 
 
