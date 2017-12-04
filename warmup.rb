b9fa27964b146273ebb47f4e7435dad1fd4f76eb
anne <richardson.ae@gmail.com>
2017-12-04 10:40:32 -0600

    WIP warmup

--- warmup.rb
+++ warmup.rb
@@ -1,26 +1,42 @@
 class Article
 
   # 1. Article.find(id)
-  def self.find(id)
+  def self.find(num)
+    "SELECT *
+    FROM articles
+    WHERE id = num
+    LIMIT 1;"
   end
 
 
   # 2. Article.first and Article.last
   def self.first
+    "SELECT *
+    FROM addresses
+    ORDER BY id ASC
+    LIMIT 1;"
   end
 
   def self.last
+    "SELECT *
+    FROM addresses
+    ORDER BY id DESC
+    LIMIT 1;"
   end
 
 
   # 3. Article.select(:column1, :column2)
   # Note that this can take any number of parameters.
-  def self.select
+  def self.select(params)
+    SELECT params
+    FROM addresses;
   end
 
 
   # 4. Article.count
   def self.count
+    "SELECT COUNT(*)
+    FROM addresses;"
   end
 
   # 5. Extend Article.find to also take an array of ids
