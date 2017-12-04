3d6ba32c738753d3636fdc575d8a5899abc39a87
anne <richardson.ae@gmail.com>
2017-12-04 13:25:29 -0600

    Complete through FakeActiveRecord::BASE

--- fake_active_record.rb
+++ fake_active_record.rb
@@ -32,13 +32,90 @@ module FakeActiveRecord
     end
 
 
+    def self.all
+      DB.execute("SELECT * FROM #{self.table_name};")
+    end
 
 
+    def self.find(id)
+      sql = "
+      SELECT *
+      FROM #{self.table_name}
+      WHERE id = #{id}
+      LIMIT 1;"
 
-    # YOUR CODE GOES HERE
+      DB.execute(sql)
+    end
 
 
+    def self.first
+      sql = "
+        SELECT *
+        FROM #{self.table_name}
+        ORDER BY id ASC
+        LIMIT 1;"
+      DB.execute(sql)
+    end
 
+
+    def self.last
+      sql = "
+        SELECT *
+        FROM #{self.table_name}
+        ORDER BY id DESC
+        LIMIT 1;"
+      DB.execute(sql)
+    end
+
+
+    def self.select(*args)
+      columns = args.join(', ')
+      sql = "
+        SELECT #{columns}
+        FROM #{self.table_name};"
+      DB.execute(sql)
+    end
+
+
+    def self.count
+      sql = "
+        SELECT COUNT(*)
+        FROM #{self.table_name};"
+      DB.execute(sql)
+    end
+
+
+    def self.find(*args)
+      ids = args.join(', ')
+
+      sql = "
+        SELECT *
+        FROM #{self.table_name}
+        WHERE id IN (#{ids});"
+      DB.execute(sql)
+    end
+
+
+    def self.where(params = {})
+      args = params.map do |k, v|
+        "#{k} = #{v}"
+      end.join("\n AND ")
+
+      sql = "SELECT * FROM #{self.table_name} WHERE " + args + ";"
+      DB.execute(sql)
+    end
+
+    def self.create(params = {})
+      columns = params.keys.join(", ")
+      values = params.values.map do |v|
+        (v.is_a? Integer) ? v : "'#{v}'"
+      end.join(", ")
+
+      sql = "
+        INSERT INTO #{self.table_name} (#{columns})
+        VALUES (#{values});"
+      DB.execute(sql)
+    end
 
 
   end
