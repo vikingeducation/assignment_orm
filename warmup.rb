3f44891d6d680383a238e8f43479759a7e4e8d77
anne <richardson.ae@gmail.com>
2017-12-04 10:05:50 -0600

    Sets up warmup file

--- warmup.rb
+++ warmup.rb
@@ -1,1 +1,36 @@
-# your code goes here
+class Article
+
+  # 1. Article.find(id)
+  def self.find(id)
+  end
+
+
+  # 2. Article.first and Article.last
+  def self.first
+  end
+
+  def self.last
+  end
+
+
+  # 3. Article.select(:column1, :column2)
+  # Note that this can take any number of parameters.
+  def self.select
+  end
+
+
+  # 4. Article.count
+  def self.count
+  end
+
+  # 5. Extend Article.find to also take an array of ids
+  def self.find(ids)
+  end
+
+  # 6. Article.where(:column1 => "value", :column2 => "value")
+  # Note that this takes a hash of parameters, and it essentially
+  # joins each constraint with an AND.
+  def self.where
+  end
+
+end
